68bec53bb81eeaf3c705354617168a7a52b01bd0
Leonel2099 <ezequielleonel1@gmail.com>
2023-03-30 15:09:55 -0300

    Correccion metodo imprimeDatos() del punto 4  y agregado de getters y setters

--- js/puntoCuatro.js
+++ js/puntoCuatro.js
@@ -1,26 +1,44 @@
 //Punto4
-class Producto{
-
-  constructor(codigo,nombre,precio){
-    this.codigo=codigo;
-    this.nombre=nombre;
-    this.precio=precio;
+class Producto {
+  #codigo;
+  #nombre;
+  #precio;
+  constructor(codigo, nombre, precio) {
+    this.#codigo = codigo;
+    this.#nombre = nombre;
+    this.#precio = precio;
   }
-
-  imprimeDatos(){
-    document.write(`Codigo: ${this.codigo}, `);
-    document.write(`Nombre: ${this.nombre}, `);
-    document.write(`Precio: ${this.precio}$ <br>`);
+  get codigo(){
+    return this.#codigo;
+  }
+  set codigo(nuevoCodigo){
+    this.#codigo = nuevoCodigo;
+  }
+  get nombre(){
+    return this.#nombre;
+  }
+  set nombre(nuevoNombre){
+    this.#nombre = nuevoNombre;
+  }
+  get precio(){
+    return this.#precio;
+  }
+  set precio(nuevoPrecio){
+    this.#precio = nuevoPrecio;
+  }
+  imprimeDatos(listaProductos) {
+    for (let key in listaProductos) {
+      document.write(`Codigo: ${listaProductos[key].codigo}, `);
+      document.write(`Nombre: ${listaProductos[key].nombre}, `);
+      document.write(`Precio: ${listaProductos[key].precio}$ <br>`);
+    }
   }
 }
 let productos = [];
-let azucar = new Producto(1,'azucar',100);
-let avena = new Producto(2,'avena',230);
-let cereal = new Producto(3,'cereal',150);
-productos.push(azucar);
-productos.push(avena);
-productos.push(cereal);
-console.log(productos)
-azucar.imprimeDatos();
-avena.imprimeDatos();
-cereal.imprimeDatos();
+let producto1 = new Producto(1, "azucar", 100);
+let producto2 = new Producto(2, "avena", 230);
+let producto3 = new Producto(3, "cereal", 150);
+productos.push(producto1);
+productos.push(producto2);
+productos.push(producto3);
+producto1.imprimeDatos(productos);
